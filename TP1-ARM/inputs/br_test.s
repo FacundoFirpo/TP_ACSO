c9dfeeb8be3a27c8d82b353f9131bea8f563ac42
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2025-03-30 17:42:03 -0300

    br test

--- TP1-ARM/inputs/br_test.s
+++ TP1-ARM/inputs/br_test.s
@@ -1,21 +1,19 @@
 .text
 // Test BR (Branch to Register) instruction
 MOVZ X1, #10     // Initialize X1 with 10
-
-// Store the address of label1 in X10
-ADR X10, label1
+MOVZ X2, #20     // Initialize X2 with 20
 
 // Branch to the address in X10
-BR X10
+BR target:
 
 // This code should be skipped
 MOVZ X2, #20     // This should be skipped
 MOVZ X3, #30     // This should be skipped
 
-label1:
-// This code should be executed after the branch
-MOVZ X4, #40     // X4 = 40
-MOVZ X5, #50     // X5 = 50
+target:
+    // This code should be executed after the branch
+    MOVZ X4, #40     // X4 = 40
+    MOVZ X5, #50     // X5 = 50
 
-// Halt
-HLT 0
+    // Halt
+    HLT 0
