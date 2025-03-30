e1af4c3e0d2a45173da9b6a42c61bc3d070f7beb
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2025-03-30 16:45:04 -0300

    br

--- TP1-ARM/inputs/br_test.s
+++ TP1-ARM/inputs/br_test.s
@@ -1,43 +1,21 @@
-.section .text
-.global br_test
+.text
+// Test BR (Branch to Register) instruction
+MOVZ X1, #10     // Initialize X1 with 10
 
-br_test:
-    // Guardar el valor de los registros que usaremos
-    stp x29, x30, [sp, -16]!  
-    mov x29, sp
+// Store the address of label1 in X10
+ADR X10, label1
 
-    // Configurar valores de prueba
-    ldr x0, =target_address  // Cargar la direccion de destino en x0
-    br x0                    // Saltar a la direccion contenida en x0
+// Branch to the address in X10
+BR X10
 
-fail:
-    mov x1, #0xDEAD          // Indicar fallo si llega aqui
-    b end
+// This code should be skipped
+MOVZ X2, #20     // This should be skipped
+MOVZ X3, #30     // This should be skipped
 
-target_address:
-    .text
-    // Test BR (Branch to Register) instruction
-    MOVZ X1, #10     // Initialize X1 with 10
+label1:
+// This code should be executed after the branch
+MOVZ X4, #40     // X4 = 40
+MOVZ X5, #50     // X5 = 50
 
-    // Store the address of label1 in X10
-    ADR X10, label1
-
-    // Branch to the address in X10
-    BR X10
-
-    // This code should be skipped
-    MOVZ X2, #20     // This should be skipped
-    MOVZ X3, #30     // This should be skipped
-
-    label1:
-    // This code should be executed after the branch
-    MOVZ X4, #40     // X4 = 40
-    MOVZ X5, #50     // X5 = 50
-
-    // Halt
-    HLT 0
-
-end:
-    // Restaurar registros y retornar
-    ldp x29, x30, [sp], 16
-    ret
+// Halt
+HLT 0
