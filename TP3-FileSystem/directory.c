e1b2a3059f2bd581e1fc4046ce5625b17ec5a7b7
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2025-05-08 11:21:27 -0300

    parte path

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -11,6 +11,40 @@
  */
 int directory_findname(struct unixfilesystem *fs, const char *name,
 		int dirinumber, struct direntv6 *dirEnt) {
-  //Implement your code here
-  return 0;
+  struct inode dir_inode;
+  if (inode_iget(fs, dirinumber, &dir_inode) < 0) {
+    return -1;
+  }
+
+  if (!(dir_inode.i_mode & IALLOC) || ((dir_inode.i_mode & IFMT) != IFDIR)) {
+    // No es un directorio válido
+    return -1;
+  }
+
+  int dir_size = inode_getsize(&dir_inode);
+  int num_entries = dir_size / sizeof(struct direntv6);
+  int entries_per_block = DISKIMG_SECTOR_SIZE / sizeof(struct direntv6);
+
+  char buf[DISKIMG_SECTOR_SIZE];
+
+  int entry_index = 0;
+  for (int block = 0; ; block++) {
+    int bytes_read = file_getblock(fs, dirinumber, block, buf);
+    if (bytes_read <= 0) break;
+
+    int entries_in_block = bytes_read / sizeof(struct direntv6);
+    struct direntv6 *entries = (struct direntv6 *)buf;
+
+    for (int i = 0; i < entries_in_block && entry_index < num_entries; i++, entry_index++) {
+      if (entries[i].d_inumber == 0)
+        continue; // Entrada vacía
+
+      // Comparar nombre (asegura terminación nula)
+      if (strncmp(entries[i].d_name, name, sizeof(entries[i].d_name)) == 0) {
+        *dirEnt = entries[i];
+        return 0;
+      }
+    }
+  }
+  return -1;
 }
--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -11,6 +11,28 @@
  * TODO
  */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    //Implement code here
-	return 0;
+    if (!pathname || pathname[0] != '/') {
+        // Solo soporta pathnames absolutos
+        return -1;
+    }
+
+    // Comenzar desde el inodo raíz (usualmente 1)
+    int curr_inumber = 1;
+    char path_copy[256];
+    strncpy(path_copy, pathname, sizeof(path_copy));
+    path_copy[sizeof(path_copy) - 1] = '\0';
+
+    // Saltar el primer '/'
+    char *token = strtok(path_copy, "/");
+    while (token != NULL) {
+        struct direntv6 entry;
+        int res = directory_findname(fs, token, curr_inumber, &entry);
+        if (res < 0) {
+            return -1; // No se encontró el componente
+        }
+        curr_inumber = entry.d_inumber;
+        token = strtok(NULL, "/");
+    }
+
+    return curr_inumber;
 }
