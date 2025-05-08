d271a26cee0222f1b75e70d83a3f7e96f567d9ea
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2025-05-08 10:01:41 -0300

    parte de inodo

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -9,7 +9,21 @@
  * TODO
  */
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-    
-    return 0;
+    struct inode in;
+    if (inode_iget(fs, inumber, &in) < 0) {
+        return -1;
+    }
+
+    int diskBlock = inode_indexlookup(fs, &in, blockNum);
+    if (diskBlock <= 0) {
+        return -1;
+    }
+
+    int res = diskimg_readsector(fs->dfd, diskBlock, buf);
+    if (res != DISKIMG_SECTOR_SIZE) {
+        return -1;
+    }
+
+    return res;
 }
 
--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -9,17 +9,41 @@
  * TODO
  */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-    //Implement Code Here
-    return 0; 
+    if (inumber < 1) return -1;
+
+    int inodes_per_block = DISKIMG_SECTOR_SIZE / sizeof(struct inode);
+    int inode_block = (inumber - 1) / inodes_per_block + INODE_START_SECTOR;
+    int inode_offset = (inumber - 1) % inodes_per_block;
+
+    struct inode buf[inodes_per_block];
+    int res = diskimg_readsector(fs->dfd, inode_block, buf);
+    if (res != DISKIMG_SECTOR_SIZE) {
+        return -1;
+    }
+
+    *inp = buf[inode_offset];
+    return 0;
 }
 
 /**
  * TODO
  */
-int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp,
-    int blockNum) {  
-        //Implement code here
-    return 0;
+ int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
+    if (!(inp->i_mode & IALLOC)) return -1;
+    if (blockNum < 0) return -1;
+
+    // Archivos pequeños (no ILARG)
+    if (!(inp->i_mode & ILARG)) {
+        if (blockNum >= 8) return -1;
+        int block = inp->i_addr[blockNum];
+        if (block == 0) return -1;
+        return block;
+    } else {
+        // Archivos grandes (ILARG): acceso indirecto
+        // No implementado aquí, pero se deja el esqueleto
+        // Se debería leer el bloque indirecto y buscar el bloque físico
+        return -1;
+    }
 }
 
 int inode_getsize(struct inode *inp) {
