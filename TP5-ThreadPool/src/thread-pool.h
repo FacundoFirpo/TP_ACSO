3cbaddea99e04b4eaab9e2024aabad461ba7be68
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2025-06-19 09:46:27 -0300

    agrego queue en .h

--- TP5-ThreadPool/src/thread-pool.h
+++ TP5-ThreadPool/src/thread-pool.h
@@ -14,6 +14,7 @@
 #include <functional>  // for the function template used in the schedule signature
 #include <thread>      // for thread
 #include <vector>      // for vector
+#include <queue>       // for queue
 #include "Semaphore.h" // for Semaphore
 
 using namespace std;
