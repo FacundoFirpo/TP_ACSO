a92cd607ddeb12fad602bf1b9ded8585732e5dd6
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2025-06-19 13:48:52 -0300

    quedaba colgado en un caso

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -18,6 +18,8 @@ ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {
 void ThreadPool::schedule(const function<void(void)>& thunk) {
     if (!thunk) throw invalid_argument("Cannot schedule nullptr function.");
 
+    if (done) throw runtime_error("Cannot schedule task after destruction.");
+
     {
         lock_guard<mutex> lg(queueLock);
         taskQueue.push(thunk);
